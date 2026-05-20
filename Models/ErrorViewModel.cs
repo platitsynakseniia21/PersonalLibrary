30f3c160c6743462c38e32cfb3a5e5ba4fc3f735
Платіцина Ксенія <kseniia.platitsyna@nure.ua>
2026-05-20 16:23:04 +0200

    Initial commit: Created ASP.NET Core MVC project structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PersonalLibrary.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
