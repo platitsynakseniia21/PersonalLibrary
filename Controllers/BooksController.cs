a04682a0eb0be3ba8cde47cc65ff2154604ca63a
Платіцина Ксенія <kseniia.platitsyna@nure.ua>
2026-05-20 22:49:22 +0200

    Додано форму створення книги

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -8,17 +8,37 @@ namespace PersonalLibrary.Controllers
     {
         private readonly BookService _bookService;
 
-        
+        // Конструктор
         public BooksController(BookService bookService)
         {
             _bookService = bookService;
         }
 
-        
+        // 1. Метод для главной страницы (список книг)
         public IActionResult Index()
         {
             var books = _bookService.GetAllBooks();
             return View(books);
+        }
+
+        // 2. Метод, который просто открывает пустую форму
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // 3. Метод, который сохраняет данные после нажатия кнопки "Зберегти"
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _bookService.AddBook(book);
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
         }
     }
 }
